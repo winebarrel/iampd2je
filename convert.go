7888873d424cc49a6d86de1f8b2c0c08408435f5
winebarrel <sugawara@winebarrel.jp>
2026-05-21 00:09:59 +0900

    WIP

--- convert.go
+++ convert.go
@@ -1,9 +1,24 @@
 package iampd2j
 
 import (
+	"errors"
+	"fmt"
 	"io"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclwrite"
 )
 
 func Convert(src []byte, filename string, out io.Writer) error {
+	f, diags := hclwrite.ParseConfig(src, filename, hcl.Pos{Line: 1, Column: 1})
+	if diags.HasErrors() {
+		return errors.Join(diags.Errs()...)
+	}
+
+	for _, block := range f.Body().Blocks() {
+		// TODO:
+		fmt.Println(block)
+	}
+
 	return nil
 }
